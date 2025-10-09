e6f5284c04b580c0fcce844a2de51c7d3ff8e8c3
samiaul <samiaul.56@gmail.com>
2025-10-09 12:28:28 +0200

    feat(Models): Improved models

--- backend/src/main/java/eu/epitech/t_dev_700/models/TeamModels.java
+++ backend/src/main/java/eu/epitech/t_dev_700/models/TeamModels.java
@@ -1,26 +1,39 @@
 package eu.epitech.t_dev_700.models;
 
+import eu.epitech.t_dev_700.models.constraints.NullableNotBlank;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class TeamModels {
 
+    public interface Team {
+        String name();
+        String description();
+    }
+
     // INTERNALS
-    public record Team(
+    public record TeamModel (
             Long id,
             String name,
             String description
-    ) {}
+    ) implements Team {}
 
     //REQUESTS
     public record PostTeamRequest(
             @NotBlank String name,
-            @NotBlank String description
-    ) {}
+            @NotNull String description
+    ) implements Team {}
 
     public record PutTeamRequest(
-            @NotEmpty String name,
-            @NotEmpty String description
-    ) {}
+            @NotBlank String name,
+            @NotNull String description
+    ) implements Team {}
+
+    public record PatchTeamRequest(
+            @NullableNotBlank String name,
+            String description
+    ) implements Team {}
 
 }
