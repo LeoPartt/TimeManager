5d71cc537fccd0c5c2d67beded931379f9231439
samiaul <samiaul.56@gmail.com>
2025-10-10 17:32:01 +0200

    feat(Refactor): Refactored controllers

--- backend/src/main/java/eu/epitech/t_dev_700/controllers/UserController.java
+++ backend/src/main/java/eu/epitech/t_dev_700/controllers/UserController.java
@@ -1,18 +1,18 @@
 package eu.epitech.t_dev_700.controllers;
 
 import eu.epitech.t_dev_700.entities.UserEntity;
+import eu.epitech.t_dev_700.models.TeamModels;
 import eu.epitech.t_dev_700.models.UserModels;
 import eu.epitech.t_dev_700.services.ClockService;
 import eu.epitech.t_dev_700.services.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-public class UserController extends CRUDController<
-        UserEntity,
+@RequiredArgsConstructor
+public class UserController implements CRUDController<
         UserModels.UserModel,
         UserModels.PostUserRequest,
         UserModels.PutUserRequest,
@@ -22,19 +22,50 @@ public class UserController extends CRUDController<
     private final UserService userService;
     private final ClockService clockService;
 
-    public UserController(UserService userService, ClockService clockService) {
-        super(userService);
-        this.userService = userService;
-        this.clockService = clockService;
+    @Override
+    @GetMapping("{id}")
+    public UserModels.UserModel Get(@PathVariable Long id) {
+        return userService.get(id);
     }
 
+    @Override
+    @GetMapping
+    public UserModels.UserModel[] GetAll() {
+        return userService.list();
+    }
+
+    @Override
+    @PostMapping
+    public UserModels.UserModel Post(@Valid @RequestBody UserModels.PostUserRequest body) {
+        return userService.create(body);
+    }
+
+    @Override
+    @PutMapping("{id}")
+    public UserModels.UserModel Put(@PathVariable Long id, @Valid @RequestBody UserModels.PutUserRequest body) {
+        return userService.replace(id, body);
+    }
+
+    @Override
+    @PatchMapping("{id}")
+    public UserModels.UserModel Patch(@PathVariable Long id, @Valid @RequestBody UserModels.PatchUserRequest body) {
+        return userService.update(id, body);
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public void Delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+    
     @GetMapping("{id}/clocks")
-    public Long[] getUserClocks(@PathVariable Long id) {
-        return clockService.getUserClocks(id);
+    public Long[] getUserClocks(@PathVariable Long id, @RequestParam("from") Long from, @RequestParam("to") Long to) {
+        return clockService.getUserClocks(id, from, to);
     }
 
     @GetMapping("/me")
     public UserModels.UserModel getUser() {
         return userService.getCurrentUser();
     }
+
 }
