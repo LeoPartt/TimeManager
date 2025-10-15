7594165d82e44c80f6813c4c7ac70257625c6f38
samiaul <samiaul.56@gmail.com>
2025-10-15 12:24:16 +0200

    test(Auth): Added tests for authorizations on routes

---
+++ backend/src/test/java/eu/epitech/t_dev_700/controllers/AbstractAuthTest.java
@@ -0,0 +1,120 @@
+package eu.epitech.t_dev_700.controllers;
+
+import eu.epitech.t_dev_700.entities.AccountEntity;
+import eu.epitech.t_dev_700.entities.UserEntity;
+import eu.epitech.t_dev_700.services.ClockService;
+import eu.epitech.t_dev_700.services.MembershipService;
+import eu.epitech.t_dev_700.services.TeamService;
+import eu.epitech.t_dev_700.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ActiveProfiles("test")
+abstract class AbstractAuthTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockitoBean
+    MembershipService membershipService;
+
+    @MockitoBean
+    UserService userService;
+
+    @MockitoBean
+    ClockService ClockService;
+
+    @MockitoBean
+    TeamService teamService;
+
+    protected UserEntity memberUser;
+    protected UserEntity managerUser;
+    protected UserEntity otherUser;
+
+
+    @BeforeEach
+    void setUp() {
+        memberUser = new UserEntity();
+        memberUser.setId(1L);
+        memberUser.setAccount(new AccountEntity());
+
+        managerUser = new UserEntity();
+        managerUser.setId(2L);
+        managerUser.setAccount(new AccountEntity());
+
+        otherUser = new UserEntity();
+        otherUser.setId(3L);
+        otherUser.setAccount(new AccountEntity());
+
+        when(membershipService.isUserManager(memberUser)).thenReturn(false);
+        when(membershipService.isUserMemberOfTeam(memberUser, 1L)).thenReturn(true);
+        when(membershipService.isUserManagerOfTeam(memberUser, 1L)).thenReturn(false);
+        when(membershipService.isUserMemberOfTeam(memberUser, 2L)).thenReturn(false);
+        when(membershipService.isUserManagerOfOther(memberUser, 2L)).thenReturn(false);
+
+        when(membershipService.isUserManager(managerUser)).thenReturn(true);
+        when(membershipService.isUserMemberOfTeam(managerUser, 1L)).thenReturn(true);
+        when(membershipService.isUserManagerOfTeam(managerUser, 1L)).thenReturn(true);
+        when(membershipService.isUserMemberOfTeam(managerUser, 2L)).thenReturn(false);
+        when(membershipService.isUserManagerOfOther(managerUser, 1L)).thenReturn(true);
+    }
+
+    // -------- helpers to build Authentication like at runtime --------
+
+    protected void doTestRequestForAuthExpectCode(
+            Authentication auth,
+            MockHttpServletRequestBuilder request,
+            ResultMatcher expected
+    ) throws Exception {
+        mvc.perform(request.with(authentication(auth)))
+                .andExpect(expected);
+    }
+
+    protected RequestPostProcessor authentication(Authentication a) {
+        return SecurityMockMvcRequestPostProcessors.authentication(a);
+    }
+
+    protected Authentication authForAdmin() {
+        return token(principal(true), new ArrayList<>());
+    }
+
+    protected Authentication authForMember() {
+        return token(principal(memberUser), new ArrayList<>());
+    }
+
+    protected Authentication authForManager() {
+        return token(principal(managerUser), new ArrayList<>());
+    }
+
+    protected UsernamePasswordAuthenticationToken token(Object principal, List<GrantedAuthority> auths) {
+        return new UsernamePasswordAuthenticationToken(principal, "N/A", auths);
+    }
+
+    protected AccountEntity principal(boolean admin) {
+        AccountEntity account = new AccountEntity();
+        if (admin) account.setFlags((byte) 0xFF);
+        return account;
+    }
+
+    protected AccountEntity principal(UserEntity user) {
+        AccountEntity account = new AccountEntity();
+        account.setUser(user);
+        return account;
+    }
+
+}
