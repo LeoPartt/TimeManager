d51210c6759707a06ff8ecf8da54a72a1439e46f
samiaul <samiaul.56@gmail.com>
2025-10-17 12:36:31 +0200

    feat(Codes): Updated response codes.

--- backend/src/main/java/eu/epitech/t_dev_700/controllers/ClockController.java
+++ backend/src/main/java/eu/epitech/t_dev_700/controllers/ClockController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,15 +20,17 @@ public class ClockController {
 
     @Operation(summary = "Record clock event")
     @PostMapping
-    public void PostClock(@Valid @RequestBody ClockModels.PostClockRequest body) {
+    public ResponseEntity<Void> PostClock(@Valid @RequestBody ClockModels.PostClockRequest body) {
         this.clockService.postClock(body);
+        return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "Record clock event for user")
     @PreAuthorize("@userAuth.isSelfOrManagerOfUser(authentication, #id)")
     @PostMapping("/{id}")
-    public void PostClock(@PathVariable Long id, @Valid @RequestBody ClockModels.PostClockRequest body) {
+    public ResponseEntity<Void> PostClock(@PathVariable Long id, @Valid @RequestBody ClockModels.PostClockRequest body) {
         this.clockService.postClock(body, id);
+        return ResponseEntity.noContent().build();
     }
 
 }
